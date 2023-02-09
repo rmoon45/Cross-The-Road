5fcfdbd964b50807d16bcbed797e6ef6e599b376
mpowell81 <mpowell81@gatech.edu>
2023-02-09 15:54:43 -0500

    Pushed S0 template for Sample Screen

---
+++ README.md
@@ -0,0 +1,7 @@
+# S0
+This is Esther Moon
+This is Madison Powell 
+This is Ashwini Tulpule
+This is Nicole Li
+This is Nikitha Shivakumar
+
--- app/src/androidTest/java/com/example/s0/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/s0/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.crosstheroad;
+package com.example.s0;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.crosstheroad", appContext.getPackageName());
+        assertEquals("com.example.s0", appContext.getPackageName());
     }
 }
