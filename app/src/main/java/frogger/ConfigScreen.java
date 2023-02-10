ba05757ea787e22555cb9a34b0ac69de4ca9ce33
atulpule6 <atulpule6@gatech.edu>
2023-02-10 17:56:15 -0500

    modified config screen with Madison

--- app/src/main/java/frogger/ConfigScreen.java
+++ app/src/main/java/frogger/ConfigScreen.java
@@ -4,11 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.s0.R;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
 public class ConfigScreen extends AppCompatActivity {
 
+    private EditText nameInput;
+    private TextView invalidName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_config_screen);
+
+        nameInput = (EditText) findViewById(R.id.nameInput);
+        invalidName = (TextView) findViewById(R.id.nameInvalid);
+
+    }
+
+    // Check if invalid.
+    public void onSetName(View view) {
+        String name = nameInput.getText().toString();
+        if (name == null || name == "" || name.trim() == "") {
+            invalidName.setVisibility(View.VISIBLE);
+        } else {
+            Player user = new Player();
+            user.setName(name);
+        }
     }
 }
---
+++ app/src/main/java/frogger/Player.java
@@ -0,0 +1,17 @@
+package frogger;
+
+public class Player {
+
+    private String name;
+    private int character;
+    private int lives;
+
+    public Player() {
+        this.name = "";
+        this.character = 0;
+        this.lives = -1;
+    }
+    public void setName(String input) {
+        name = input;
+    }
+}
