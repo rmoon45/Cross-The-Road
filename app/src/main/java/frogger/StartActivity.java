37e3b994fb53822d215131b33dbaafe5dd61f2a7
nshivakumar6 <nshivakumar6@gatech.edu>
2023-02-10 17:36:30 -0500

    adding startActivity and startxml files

---
+++ app/src/main/java/frogger/StartActivity.java
@@ -0,0 +1,31 @@
+package frogger;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+import com.example.s0.R;
+import android.content.Intent;
+
+public class StartActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start);
+
+    }
+    public void onEnd(View view) {
+        finish();
+    }
+    public void onStart(View view) {
+           Intent intent=new Intent(StartActivity.this,ConfigScreen.class);
+           startActivity(intent);
+
+    }
+
+}
