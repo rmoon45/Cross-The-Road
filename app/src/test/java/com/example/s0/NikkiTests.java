728e82e358d6b8abb3e2354cc4da14ab6aefbc66
nshivakumar6 <nshivakumar6@gatech.edu>
2023-02-22 21:17:48 -0500

    added another JUNIT test for button background

--- app/src/test/java/com/example/s0/NikkiTests.java
+++ app/src/test/java/com/example/s0/NikkiTests.java
@@ -7,6 +7,8 @@ import frogger.ConfigScreen;
 import frogger.Game;
 import frogger.GameScreen;
 import frogger.Player;
+
+import android.graphics.Color;
 import android.view.View;
 public class NikkiTests {
     Game game = new Game();
@@ -24,5 +26,24 @@ public class NikkiTests {
         user.setName(null);
         assertTrue(configScreen.getInvalidName().getVisibility()==View.VISIBLE );
     }
+    public void checkEasyBackground(){
+        configScreen.getEasy().performClick();
+        assertEquals(configScreen.getEasy().getBackground(), Color.BLUE);
+        assertEquals(configScreen.getMedium().getBackground(), -7829368);
+        assertEquals(configScreen.getHard().getBackground(), -7829368);
+    }
+    public void checkMediumBackground(){
+        configScreen.getMedium().performClick();
+        assertEquals(configScreen.getMedium().getBackground(), Color.BLUE);
+        assertEquals(configScreen.getEasy().getBackground(), -7829368);
+        assertEquals(configScreen.getHard().getBackground(), -7829368);
+    }
+    public void checkHardBackground(){
+        configScreen.getHard().performClick();
+        assertEquals(configScreen.getHard().getBackground(), Color.BLUE);
+        assertEquals(configScreen.getEasy().getBackground(), -7829368);
+        assertEquals(configScreen.getMedium().getBackground(), -7829368);
+    }
+
 
 }
