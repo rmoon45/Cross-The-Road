03ef9c87c1bf93105ae0b484d1217022523303e1
nli307 <nli307@gatech.edu>
2023-04-02 11:27:11 -0400

    no more Game

--- app/src/test/java/com/example/s0/GameTests.java
+++ app/src/test/java/com/example/s0/GameTests.java
@@ -6,35 +6,58 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
-import android.content.Context;
-import android.os.Looper;
+import android.view.View;
 
-import frogger.Game;
+import java.lang.reflect.Field;
+
+import frogger.ConfigScreen;
 
 public class GameTests {
-    private Game game = null;
-    private Looper looper = null;
+    private ConfigScreen configScreen = mock(ConfigScreen.class, CALLS_REAL_METHODS);
 
     @Before
     public void setup() {
-        looper = mock(Looper.class);
-        Context context = mock(Context.class);
-        when(context.getMainLooper()).thenReturn(looper);
-        game = new Game();
+        doCallRealMethod().when(configScreen).onEasySelected((View) any());
+        doCallRealMethod().when(configScreen).onMediumSelected((View) any());
+        doCallRealMethod().when(configScreen).onHardSelected((View) any());
     }
 
     // Nicole: Selecting a certain difficulty changes the number of lives the player has
     @Test
     public void testDifficultyLives() {
-        game.setDifficulty("easy");
-        int easyLives = game.getLives();
+        try {
+            configScreen.onEasySelected(mock(View.class));
+        } catch (Exception e) {
+            System.out.println("harmless error, this is fine");
+        }
+        int easyLives = (int) getField(configScreen, "lives");
 
-        game.setDifficulty("medium");
-        int mediumLives = game.getLives();
+        try {
+            configScreen.onMediumSelected(mock(View.class));
+        } catch (Exception e) {
+            System.out.println("harmless error, this is fine");
+        }
+        int mediumLives = (int) getField(configScreen, "lives");
 
-        game.setDifficulty("hard");
-        int hardLives = game.getLives();
+        try {
+            configScreen.onHardSelected(mock(View.class));
+        } catch (Exception e) {
+            System.out.println("harmless error, this is fine");
+        }
+        int hardLives = (int) getField(configScreen, "lives");
 
         assertFalse(easyLives == mediumLives || mediumLives == hardLives || easyLives == hardLives);
+    }
+
+    private <T> Object getField(T object, String fieldName) {
+        try {
+            Field field = object.getClass().getSuperclass().getDeclaredField(fieldName);
+            field.setAccessible(true);
+            return field.get(object);
+        } catch (NoSuchFieldException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
